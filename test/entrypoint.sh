95cd9bf8dcf7adf3a81d0fca33ca449ede124063
xorphitus <xorphitus@gmail.com>
2026-04-04 22:59:01 +0900

    chore(test): update Dockerfile, compose and entrypoint for test environment, reduce image size and add memory limits

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -7,11 +7,11 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 PROJECT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
 
 # ---------------------------------------------------------------------------
-# 1. Create two loopback btrfs images (512 MB each)
+# 1. Create two loopback btrfs images (64 MB each)
 # ---------------------------------------------------------------------------
-truncate -s 512M /tmp/disk_a.img /tmp/disk_b.img
-mkfs.btrfs -f /tmp/disk_a.img
-mkfs.btrfs -f /tmp/disk_b.img
+truncate -s 64M /tmp/disk_a.img /tmp/disk_b.img
+mkfs.btrfs -f -M -m single /tmp/disk_a.img
+mkfs.btrfs -f -M -m single /tmp/disk_b.img
 mkdir -p /mnt/A /mnt/B
 mount -o loop /tmp/disk_a.img /mnt/A
 mount -o loop /tmp/disk_b.img /mnt/B
